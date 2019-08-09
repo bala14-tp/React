61cd22d1ced90b395b40fd18ab763dff70a68ad7
balachanther <bala@irslogics.com>
2019-08-09 20:54:27 +0530

    Post Section Completed

--- models/post.js
+++ models/post.js
@@ -20,7 +20,6 @@ const postSchema = new mongoose.Schema({
             type: mongoose.Schema.Types.ObjectId,
             ref: 'user'
         }
-
     }],
     comments: [{
         user: {
--- routes/api/posts.js
+++ routes/api/posts.js
@@ -71,4 +71,94 @@ router.delete('/deletePost/:delID', auth, async (req, res) => {
     }
 })
 
+router.put('/like/:pid', auth, async (req, res) => {
+
+    try {
+
+        let post = await Post.findOne({ _id: req.params.pid });
+
+        let likes = post.likes.filter(x => x.user.toString() === req.user.id);
+        if (likes.length > 0) {
+            return res.send("Post already liked")
+        }
+        else {
+            post.likes.unshift({ user: req.user.id });
+            await post.save();
+            return res.send("Post liked");
+        }
+
+    } catch (error) {
+        res.send("Something went wrong");
+    }
+})
+
+router.delete('/unlike/:pid', auth, async (req, res) => {
+    try {
+        let post = await Post.findOne({ _id: req.params.pid });
+
+        let likes = post.likes.filter(x => x.user.toString() === req.user.id);
+
+        if (likes.length > 0) {
+            post.likes.splice(0, 1);
+            post.save();
+            return res.send("Likes removed");
+        }
+        else {
+            return res.send("Post already unliked")
+        }
+
+    } catch (error) {
+        res.send("Something went wrong");
+    }
+});
+
+
+router.put('/comments/:pid',
+    [
+        auth,
+        [
+            check('text', 'Please enter text').not().isEmpty()
+        ]
+    ],
+    async (req, res) => {
+
+        const error = validationResult(req);
+
+        if (!error.isEmpty()) {
+            return res.status(400).json(error.array());
+        }
+
+        try {
+            let post = await Post.findOne({ _id: req.params.pid });
+
+            let user = await User.findOne({ _id: req.user.id });
+
+            post.comments.unshift({ user: req.user.id, text: req.body.text, name: user.name, avatar: user.avatar });
+            await post.save();
+            res.send("Comments Saved!");
+
+        } catch (error) {
+            res.send("Something went wrong");
+        }
+    })
+
+router.delete('/delcomment/:pid/:cid', auth, async (req, res) => {
+    try {
+        let post = await Post.findOne({ _id: req.params.pid });
+        let cindex = post.comments.map(x => x._id).indexOf(req.params.cid);
+        
+        if (cindex !== -1) {
+            post.comments.splice(cindex, 1);
+            await post.save();
+            return res.send("Comments Deleted");
+        }
+        else {
+            return res.send("No comments avilable");
+        }
+    } catch (error) {
+        res.send("Something went wrong");
+    }
+})
+
+
 module.exports = router;
